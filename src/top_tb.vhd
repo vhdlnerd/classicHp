377a99f525fddebc7fa2f8fdb3f485bd91a4c104
VHDL Nerd <vhdlnerd@comcast.net>
2014-04-01 21:23:27 -0700

    Added license info

--- src/top_tb.vhd
+++ src/top_tb.vhd
@@ -1,10 +1,34 @@
+----------------------------------------------------------------------------------
+-- The MIT License (MIT)
+-- 
+-- Copyright (c) 2014 Brian K. Nemetz
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+----------------------------------------------------------------------------------
+
 --------------------------------------------------------------------------------
 -- Company: 
--- Engineer:
+-- Engineer: Brian Nemetz
 --
 -- Create Date:   10:56:50 10/16/2012
 -- Design Name:   
--- Module Name:   F:/hwHP/classicHp/top_tb.vhd
+-- Module Name:   top_tb.vhd
 -- Project Name:  classicHp
 -- Target Device:  
 -- Tool versions:  
@@ -25,6 +49,12 @@
 -- to guarantee that the testbench will bind correctly to the post-implementation 
 -- simulation model.
 --------------------------------------------------------------------------------
+
+--
+--  WARNING: I have not used htis test bench in a long time. I'm not sure if its
+--           working any longer.  BKN 4/1/2014
+--
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
